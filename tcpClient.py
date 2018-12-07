03e9f46bdb97ac17640035f6365be1c944e306f4
Chris Arnold <45668642+ICSChris@users.noreply.github.com>
2018-12-07 10:19:53 -0800

    Update tcpClient.py

--- tcpClient.py
+++ tcpClient.py
@@ -1,13 +1,13 @@
 import socket
 
-targetHost = "127.0.0.1"
-targetPort = 2600
+target_host = "127.0.0.1"
+target_port = 2600
 
 # Create a socket object and pass to client variable
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the client
-client.connect((targetHost,targetPort))
+client.connect((target_host,target_port))
 
 # Transmit data
 client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
